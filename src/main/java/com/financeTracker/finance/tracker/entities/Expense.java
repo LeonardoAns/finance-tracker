74bf4ccb1938a01280b4774b83c2359e3459d274
LeonardoAns <leonardianschauq@gmail.com>
2024-09-19 17:42:56 -0300

    update expense request

--- src/main/java/com/financeTracker/finance/tracker/entities/Expense.java
+++ src/main/java/com/financeTracker/finance/tracker/entities/Expense.java
@@ -1,15 +1,11 @@
 package com.financeTracker.finance.tracker.entities;
 
-import com.fasterxml.jackson.databind.annotation.EnumNaming;
 import com.financeTracker.finance.tracker.entities.enums.PaymentMethod;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
