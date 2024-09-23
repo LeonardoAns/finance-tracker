2bdc8e2883f47209d99b3fb54d677e6d76308b3d
LeonardoAns <leonardianschauq@gmail.com>
2024-09-23 17:44:36 -0300

    .

--- src/main/java/com/financeTracker/finance/tracker/repositories/UserRepository.java
+++ src/main/java/com/financeTracker/finance/tracker/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.financeTracker.finance.tracker.repositories;
 import com.financeTracker.finance.tracker.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
     Optional<UserEntity> findByVerificationCode(String verificationCode);
