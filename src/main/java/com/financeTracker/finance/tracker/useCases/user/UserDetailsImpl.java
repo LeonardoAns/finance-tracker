1fd7541362d6285491011d94b3c2f1fcaac104be
LeonardoAns <leonardianschauq@gmail.com>
2024-09-23 18:24:25 -0300

    ...

--- src/main/java/com/financeTracker/finance/tracker/useCases/user/UserDetailsImpl.java
+++ src/main/java/com/financeTracker/finance/tracker/useCases/user/UserDetailsImpl.java
@@ -2,16 +2,17 @@ package com.financeTracker.finance.tracker.useCases.user;
 
 import com.financeTracker.finance.tracker.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserDetailsImpl implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
